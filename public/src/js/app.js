8e3c4f73ac6ac387ad67cb36a22094b7e4845f7f
Babar Waheed <babar.waheed@gmail.com>
2019-12-02 00:08:02 +1100

    Polyfills (for legacy browser support)

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,10 +1,17 @@
 var deferredPrompt;
 
+if(!window.Promise){
+    window.Promise = Promise;
+}
+
 if('serviceWorker' in navigator){
     navigator.serviceWorker
         .register('/sw.js')
         .then(function(){
             console.log('Service Worker Registered!');
+        })
+        .catch(function(err){
+            console.log(err);
         })
 }
 
