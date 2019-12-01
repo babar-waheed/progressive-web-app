54f18945933754ed9298775e423c0e0f66448823
Babar Waheed <babar.waheed@gmail.com>
2019-12-02 10:46:35 +1100

    caching app.js using cache api in service worker

--- public/sw.js
+++ public/sw.js
@@ -1,13 +1,30 @@
 self.addEventListener('install', function(event){
-    console.log('[Service Worker Installing]', event);
+    //console.log('[Service Worker Installing]', event);
+
+    event.waitUntil(
+        caches.open('static')
+        .then(function(cache){
+            console.log('[service worker install]');
+            cache.add('/src/js/app.js');
+        })
+    );
 })
 
 self.addEventListener('activate', function(event){
-    console.log('[Service Worker Activating]', event);
+    //console.log('[Service Worker Activating]', event);
     return self.clients.claim();
 })
 
 self.addEventListener('fetch', function(event) {
-    console.log('[Service Worker] Fetching something ....', event);
-    event.respondWith(fetch(event.request));
+    //console.log('[Service Worker] Fetching something ....', event);
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response){
+                if(response){
+                    return response;
+                }else{
+                    return fetch(event.request);
+                }
+            })
+    );
  });
