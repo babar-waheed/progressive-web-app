d828dd1a6e8648c56655a75c5086fad2d95d62b4
Babar Waheed <babar.waheed@gmail.com>
2019-12-02 16:14:27 +1100

    better way of parsing static cache URLS

--- public/sw.js
+++ public/sw.js
@@ -1,27 +1,40 @@
 var STATIC_CACHE = 'static-v6';
 var DYNAMIC_CACHE = 'dynamic';
+var STATIC_FILES = [
+    '/',
+    '/index.html',
+    '/offline.html',
+    '/src/js/app.js',
+    '/src/js/feed.js',
+    '/src/js/promise.js',
+    '/src/js/fetch.js',
+    '/src/js/material.min.js',
+    '/src/css/app.css',
+    '/src/css/feed.css',
+    '/src/images/main-image.jpg',
+    'https://fonts.googleapis.com/css?family=Roboto:400,700',
+    'https://fonts.googleapis.com/icon?family=Material+Icons',
+    'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
+];
+
+function isInArray(string, array) {
+    var cachePath;
+    if (string.indexOf(self.origin) === 0) { // request targets domain where we serve the page from (i.e. NOT a CDN)
+      console.log('matched ', string);
+      cachePath = string.substring(self.origin.length); // take the part of the URL AFTER the domain (e.g. after localhost:8080)
+    } else {
+      cachePath = string; // store the full request (for CDNs)
+    }
+    return array.indexOf(cachePath) > -1;
+  }
+
 self.addEventListener('install', function(event) {
     console.log('[Service Worker] Installing Service Worker ...', event);
     event.waitUntil(
       caches.open(STATIC_CACHE)
         .then(function(cache) {
           console.log('[Service Worker] Precaching App Shell');
-          cache.addAll([
-            '/',
-            '/index.html',
-            '/offline.html',
-            '/src/js/app.js',
-            '/src/js/feed.js',
-            '/src/js/promise.js',
-            '/src/js/fetch.js',
-            '/src/js/material.min.js',
-            '/src/css/app.css',
-            '/src/css/feed.css',
-            '/src/images/main-image.jpg',
-            'https://fonts.googleapis.com/css?family=Roboto:400,700',
-            'https://fonts.googleapis.com/icon?family=Material+Icons',
-            'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
-          ]);
+          cache.addAll(STATIC_FILES);
         })
     )
   });
@@ -57,6 +70,11 @@ self.addEventListener('fetch', function (event) {
               });
           })
       );
+
+    } else if (isInArray(event.request.url, STATIC_FILES)){  
+        event.respondWith(
+            caches.match(event.request)
+        );
     } else {
       event.respondWith(
         caches.match(event.request)
