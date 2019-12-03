4c6cdd5bfb0cf8b6eac44b8fa10014fc3a8970a5
Babar Waheed <babar.waheed@gmail.com>
2019-12-03 13:42:44 +1100

    Push Notification Reqest Permission

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,5 +1,7 @@
 
 var deferredPrompt;
+var enableNotificationsButton = document.querySelectorAll('.enable-notifications');
+
 
 if (!window.Promise) {
   window.Promise = Promise;
@@ -22,3 +24,32 @@ window.addEventListener('beforeinstallprompt', function(event) {
   deferredPrompt = event;
   return false;
 });
+
+function displayConfirmNotification() {
+  var options = {
+    body: 'You successfully subscribe to our Notification Service'
+  }
+  new Notification('Successfully subscribed!', options);
+}
+
+function askForNotification(){
+  console.log('button cliked');
+  Notification.requestPermission(function(result){
+    console.log('User Choice', result);
+    if(result !== 'granted'){
+      console.log('No notification permission granted');
+      displayConfirmNotification()
+    }else{
+
+    }
+  });
+}
+
+if('Notification' in window){
+  console.log("%cNotification in Window", "color:orange", enableNotificationsButton.length);
+  for(var i=0; i < enableNotificationsButton.length; i++){
+    enableNotificationsButton[i].style.display = 'block';
+    enableNotificationsButton[i].addEventListener('click', askForNotification)
+  }
+  
+}
